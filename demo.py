e68ce2955928bb6d600118542ab98b13298eab86
prathiprashanthi <pprashanthi169@gmail.com>
2022-08-19 14:50:13 +0530

    prashanthi file update

--- demo.py
+++ demo.py
@@ -5,4 +5,14 @@ def myfunc():
 print("demo module name is:",__name__)
 print(dir(p))
 
+n1=int(input("enter the number1:"))
+n2=int(input("enter the number2:"))
+n3=int(input("enter the number3:"))
+if n1>n2 and n1>n3:
+    largest =n1
+elif n2>n1 and n2>n3:
+    largest =n2
+else:
+    largest =n3
+print("Largest among {}{} and {} is {}".format(n1,n2,n3,largest))
 
